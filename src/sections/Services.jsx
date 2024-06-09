6c8759f90408e5971e2c4e803d88f9349f5f3775
Jephthah Dominic <stdominicjephthah63@gmail.com>
2024-06-09 21:16:15 +0100

    Created the brand description sections

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -5,7 +5,7 @@ import ServiceCard from '../components/ServiceCard'
 const Services = () => {
   return (
     <section id='services' className='max-container flex flex-col gap-4'>
-      <h2 className='font-montserrat text-center text-3xl max-sm:text-md max-xl:text-left text-white text-semibold'>Our Services</h2>
+      <h2 className='font-montserrat text-center text-4xl max-sm:text-md max-xl:text-left text-white text-bold'>Our Services</h2>
       <p className='text-center max-xl:text-left text-xl text-white-400 font-roboto leading-normal'>We are committed to giving you the value you deserve</p>
       <div className='flex items-center gap-4 flex-wrap max-lg:flex-col'>
         {services.map((service)=>(
