f0407a6e6fae6c17547fb87deba0b2bd840a2c9d
Jephthah Dominic <75135698+jephthahdominic@users.noreply.github.com>
2024-06-27 23:16:27 +0100

    Update Services.jsx

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
   return (
     <section id='services' className='max-container' data-aos-once="false" data-aos="fade-right" data-aos-offset="100" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="50">
       <h2 className='font-montserrat text-4xl max-sm:text-md text-center text-bold text-white'>Our Core Values</h2>
-      <p className='text-xl mt-2 max-sm:text-md font-roboto text-white-400 text-center'>We are committed to adding value to the world</p>
+      <p className='text-xl mt-1 max-sm:text-md font-roboto text-white-400 text-center'>We are committed to adding value to the world</p>
       <div className='mt-4 flex items-center justify-center xl:gap-9 gap-4 max-xl:flex-col flex-wrap'>
         {services.map((service)=>(
             <ServiceCard 
