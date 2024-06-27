7df984b0d33effa5c9863d2f1f93be47728f3ffc
Jephthah Dominic <75135698+jephthahdominic@users.noreply.github.com>
2024-06-27 23:27:01 +0100

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -21,7 +21,7 @@ const SideNav = ({openSideBar, sideBarState}) => {
         <ul className='w-full mt-7 flex flex-col gap-5 pt-3 pb-3'>
           {navLinks.map((link)=>(
             <li key={link.label} className='w-full p-2 flex justify-between items-center hover:bg-white-400' onClick={()=>openSideBar(false)}>
-              <a href={link.url} className='font-montserrat text-black text-2xl font-semibold'>{link.label}</a>
+              <a href={link.url} className='font-montserrat text-black text-xl font-semibold'>{link.label}</a>
             </li>
           ))}
         </ul>
@@ -29,7 +29,7 @@ const SideNav = ({openSideBar, sideBarState}) => {
           <div className='flex gap-4'>
             {socialMedia.map((social)=>(
               <a href={social.url} className='hover:cursor-pointer bg-white border border-slate-gray rounded-full p-2 transition-all hover:bg-transparent hover:border-white'>
-                <img src={social.imgURL} alt={social.alt} width={40} height={40}/>
+                <img src={social.imgURL} alt={social.alt} width={24} height={24}/>
               </a>
             ))}
           </div>
