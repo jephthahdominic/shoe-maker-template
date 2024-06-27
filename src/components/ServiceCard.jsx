1f8ce711bd8c56d41ea944de57047e48f5744d21
Jephthah Dominic <75135698+jephthahdominic@users.noreply.github.com>
2024-06-27 22:51:31 +0100

    Update ServiceCard.jsx

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -11,7 +11,7 @@ const ServiceCard = ({icon, service, subText}) => {
                 className='w-[150px] h-[150px] bg-white rounded-full m-auto object-contain'
             />
             <p className='mt-2 text-xl max-xl:text-md text-white leading-normal font-montserrat'>{service}</p>
-<p className="text-white-400 max-xl:text-sm info-text font-roboto">{subText}</p>
+<p className="text-white-400 max-xl:text-sm info-text font-roboto leading-normal">{subText}</p>
         </div>
     </div>  
   )
