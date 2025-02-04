914a5932c43cba34ede2e15f4da9291940d9e453
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-04 12:04:10 +0100

    worked on the layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import NavLinks from './NavLinks'
+import LinkToLogin from './LinkToLogin';
 
 const Navbar = ({openSideBar}) => {
     const [headerFixed, setHeaderFixed] = useState(false);
@@ -14,12 +15,15 @@ const Navbar = ({openSideBar}) => {
     return (
         <header className={`padding-x py-6 z-50 w-full fixed ${headerFixed && 'bg-darkTransparent'}`}>
             <nav className='flex justify-between items-center max-container'>
-                <div className='flex items-center max-xl:flex-row-reverse max-lg:w-full justify-between sm:gap-4'>
+                <div className='flex items-center gap-4'>
                     <GiHamburgerMenu
                         className='text-white text-3xl hidden max-lg:flex'
                         onClick={()=>openSideBar(true)}
                     />
                     <h1 className='text-white font-bold text-3xl font-poppins leading-normal tracking-widest'>Ellech</h1>
+                </div>
+                <div className='lg:hidden'>
+                    <LinkToLogin>Sign In</LinkToLogin>
                 </div>
                 <div className='flex items-center gap-4 max-lg:hidden'>
                     <NavLinks />
