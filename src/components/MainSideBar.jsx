d173a346d7df22dd03300b519f9ac4235e51058e
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-04 14:36:04 +0100

    working on the home page

--- src/components/MainSideBar.jsx
+++ src/components/MainSideBar.jsx
@@ -7,7 +7,7 @@ export default function MainSideBar() {
     const {openSidebar, ToggleSideBar} = useSideBar();
   return (
     <section className={`${!openSidebar && 'hidden' } fixed top-0 left-0 h-screen w-full bg-[rgba(0,0,0,0.1)]`}>
-        <div className={`w-1/2 h-screen left-0 top-0 padding-y bg-white flex flex-col items-center`} onMouseOut={ToggleSideBar}>
+        <div className={`w-1/2 max-sm:w-full h-screen left-0 top-0 padding-y bg-white flex flex-col items-center`} onMouseOut={ToggleSideBar}>
             <LinkToLogin>Login / Register</LinkToLogin>
             <nav className='w-full mt-8'>
                 <AppNav />
