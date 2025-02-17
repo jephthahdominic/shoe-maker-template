ec0a5fef3d92be8c16a4abdb0b6eed8f3d6348b0
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-17 13:11:55 +0100

    working on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ export default function Home() {
             <section className='mt-14'>
                 <div className='flex justify-between items-center'>
                     <h2 className='font-playfair text-[26px] font-bold'>Coporate shoes</h2>
-                    <Link className='text-[22px] text-darkTransparent'>More</Link>
+                    <Link className='text-[22px] text-darkTransparent'>more</Link>
                 </div>
                 <div className='w-full flex items-center gap-4 mt-3 overflow-auto'>
                     {
@@ -46,7 +46,7 @@ export default function Home() {
             <section className='mt-14'>
                 <div className='flex justify-between items-center'>
                     <h2 className='font-playfair text-[26px] font-bold'>Coporate shoes</h2>
-                    <Link className='text-[22px] text-darkTransparent'>More</Link>
+                    <Link className='text-[22px] text-darkTransparent'>more</Link>
                 </div>
                 <div className='w-full flex items-center gap-4 mt-3 overflow-auto'>
                     {
