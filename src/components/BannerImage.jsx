d78ea24a6352d4378b8fd2955642403df010e93a
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-17 13:18:54 +0100

    working on the home page

--- src/components/BannerImage.jsx
+++ src/components/BannerImage.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function BannerImage({src, alt}) {
   return (
-    <img src={src} alt={alt} className='w-[100%] h-[100%]'/>
+    <img src={src} alt={alt} className='w-[100%] h-[300px] object-cover'/>
   )
 }
