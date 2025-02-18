cc7c63acd01bd62ab43727ef2273424afd29bc77
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-18 12:09:44 +0100

    Worked on the sidebar context

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,14 +1,16 @@
 import { FiMenu, FiShoppingCart } from 'react-icons/fi'
 import Logo from './Logo'
 import { Link } from 'react-router-dom'
+import { useSidebar } from '../contexts/SidebarContext'
 
 export default function AppHeader() {
+  const {setIsSidebarOpen} = useSidebar();
   return (
     <header className='w-full p-3 bg-white shadow-md flex items-center justify-between'>
         <Logo />
         <div className='flex gap-6 text-[24px]'>
             <Link to='cart'><FiShoppingCart /></Link>
-            <button><FiMenu /></button>
+            <button onClick={()=>setIsSidebarOpen((s)=>!s)}><FiMenu /></button>
         </div>
     </header>
   )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { FiUser } from 'react-icons/fi'
 import { IoClose } from 'react-icons/io5'
 import { Link } from 'react-router-dom'
+import { useSidebar } from '../contexts/SidebarContext'
 
 export default function SideBar() {
-    
+    const {isSidebarOpen, setIsSidebarOpen} = useSidebar()
   return (
-    <div className='w-full h-screen flex justify-end absolute z-40 top-0 bg-[rgba(0,0,0,0.37)]'>
+    <div className={`w-full h-screen flex justify-end absolute z-40 top-0 bg-[rgba(0,0,0,0.37)] ${!isSidebarOpen && 'hidden'}`}>
         <nav className='bg-white h-full w-[80%] -mr-[300px]'>
             <header className='px-4 py-4 w-full relative border-b-[3px] border-b-[3px solid E5E7EB]'>
-                <IoClose className='text-[28px] absolute right-4'/>
+                <IoClose className='text-[28px] absolute right-4' onClick={()=>setIsSidebarOpen(s=>!s)}/>
                 <div className='mt-8'>
                     <Link to='signin' className='flex items-center gap-2 font-poppins font-normal text-xl'>
                         <FiUser className=' text-2xl'/> Sign in
