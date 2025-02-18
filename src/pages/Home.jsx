2fb8c61912ad9f338513e9d34c134aff1d21edc9
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-18 22:52:01 +0100

    Working on the searchbar context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
                 <BannerImage src={elech} alt={'banner'}/>
             </Banner>
             <section className='mt-10'>
-                <h2 className='font-playfair text-[1.5rem] font-semibold'>Top Orders</h2>
+                <h2 className='font-playfair text-[2rem] font-semibold'>Top Orders</h2>
                 <div className='w-full flex items-center gap-4 mt-3 overflow-auto'>
                     {
                         shoes.map((shoe, key)=>(
@@ -34,7 +34,7 @@ export default function Home() {
             </section>
             <section className='mt-14'>
                 <div className='flex justify-between items-center'>
-                    <h2 className='font-playfair text-[1.5rem] font-semibold'>Coporate shoes</h2>
+                    <h2 className='font-playfair text-[2rem] font-semibold'>Coporate shoes</h2>
                     <Link className='text-[1rem] text-darkTransparent'>more</Link>
                 </div>
                 <div className='w-full flex items-center gap-4 mt-3 overflow-auto'>
@@ -47,7 +47,7 @@ export default function Home() {
             </section>
             <section className='mt-14'>
                 <div className='flex justify-between items-center'>
-                    <h2 className='font-playfair text-[1.5rem] font-semibold'>Coporate shoes</h2>
+                    <h2 className='font-playfair text-[2rem] font-semibold'>Coporate shoes</h2>
                     <Link className='text-[1rem] text-darkTransparent'>more</Link>
                 </div>
                 <div className='w-full flex items-center gap-4 mt-3 overflow-auto'>
