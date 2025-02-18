26628530f7193053489db00185f905e58e4c28de
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-18 12:55:37 +0100

    Working on the searchbar context

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Home from './pages/Home'
 import { SideBarProvider } from './contexts/SidebarContext'
+import { SearchBarProvider } from './contexts/SearchBarContext'
 
 export default function App() {
   return (
     <SideBarProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />}/>
-        </Routes>
-      </BrowserRouter>
+      <SearchBarProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />}/>
+          </Routes>
+        </BrowserRouter>
+      </SearchBarProvider>
     </SideBarProvider>
   )
 }
