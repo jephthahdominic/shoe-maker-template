810a2653ae160e595ba4f496382ce1b83db2cb8a
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-02-18 22:49:10 +0100

    Working on the searchbar context

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Logo() {
-  return <Link to='/'><h1 className='font-playfair text-[32px] font-bold'>Elech</h1></Link>
+  return <Link to='/'><h1 className='font-playfair text-[2rem] font-bold'>Elech</h1></Link>
 }
