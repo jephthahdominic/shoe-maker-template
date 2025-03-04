e6958e60dba206ca3ea8b3287b527bebe8cd79da
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-03-04 04:36:34 -0800

    made the actions div fixed when scrolling happens

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,7 +5,7 @@ export default function Reviews() {
   return (
     <div className='p-3 mt-6'>
         <header className='flex items-center justify-between'>
-          <h2 className='font-sans font-semibold text-[1.5rem]'>Reviews</h2>
+          <h2 className='font-sans font-regular text-[1.5rem]'>Reviews</h2>
           <Ratings />
         </header>
     </div>
